1db951446d12947400d49df0ad37b441f4145866
Victor Vermot <victorvermot@gmail.com>
2025-08-04 13:33:45 +0200

    better shooting feedback

--- Assets/Scripts/Managers/score_manager.gd
+++ Assets/Scripts/Managers/score_manager.gd
@@ -10,3 +10,15 @@ func _ready() -> void:
 func on_score_received(score_value: int) -> void:
 	current_score += score_value
 	score_label.text = str(current_score)
+	
+	# TODO Should equilibrate this value right now it is always 1
+	var intensity = clamp(score_value / 10.0, 1.0, 2.0)
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+
+	tween.tween_property(score_label, "scale", Vector2(1.2, 1.2) * intensity, 0.1)
+
+	tween.tween_property(score_label, "rotation_degrees", randf_range(-10, 10) * intensity, 0.05)
+	tween.tween_property(score_label, "rotation_degrees", 0, 0.05)
+
+	tween.tween_property(score_label, "scale", Vector2.ONE, 0.1)
