c67c679ffa7378aff9c6fc2ee0d7f9e753a19797
Victor Vermot <victorvermot@gmail.com>
2025-08-04 16:09:45 +0200

    add rotation

--- Assets/Scripts/Player/raquette.gd
+++ Assets/Scripts/Player/raquette.gd
@@ -12,18 +12,24 @@ func rotate_raquette() -> void:
 
 	if shot_direction.x < 0:
 		is_facing_right = false
-		rotation_degrees = 40
+		switch_rotation(40)
 	else:
 		is_facing_right = true
-		rotation_degrees = -40
+		switch_rotation(-40)
+
+func switch_rotation(rotation_target: float):
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", rotation_target, 0.1)
 
 func play_prepare_raquette_shoot_anim() -> void:
 	var target_skew = deg_to_rad(-60) if is_facing_right else deg_to_rad(60)
 	var tween = create_tween()
 	tween.tween_property(self, "skew", target_skew, 0.1)
+	tween.tween_property(self, "scale", scale + Vector2(0.05, 0.0), 0.1)
 
 func play_raquette_shoot_anim() -> void:
 	var target_skew = deg_to_rad(60) if is_facing_right else deg_to_rad(-60)
 	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(0.5, 0.5), 0.1)
 	tween.tween_property(self, "skew",target_skew, 0.1)
 	tween.tween_property(self, "skew", deg_to_rad(0), 0.2)
