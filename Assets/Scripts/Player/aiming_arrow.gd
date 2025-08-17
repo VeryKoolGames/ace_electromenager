8e6103135253bf2f220d41204b4ddb2918c7c4de
Vermisso <60429652+Vermisso@users.noreply.github.com>
2025-08-17 16:25:04 +0200

    flammes et tout

--- Assets/Scripts/Player/aiming_arrow.gd
+++ Assets/Scripts/Player/aiming_arrow.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name AimingArrow
 
-@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+@onready var gpu_particles_2d: GPUParticles2D = $Piques2
 @onready var strength_texture_bar: TextureProgressBar = $Mask/StrengthTextureBar
 @export var fill_speed := 0.0
 var fill_direction = 1
